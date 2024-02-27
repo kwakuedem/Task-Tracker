8d280e5a6f27e3f5c4bbd187edb121d8a2af1738
Edem Kwaku <edemkwakuavorley@gmail.com>
2024-02-27 07:00:00 -0800

    Added a tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=[('todo', 'To-Do'), ('in_progress', 'In Progress'), ('completed', 'Completed')])
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.title
