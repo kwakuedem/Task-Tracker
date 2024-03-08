325f479a26192b627e9e6da79d6368ea13733cb3
Edem Kwaku <edemkwakuavorley@gmail.com>
2024-03-08 02:29:06 -0800

    changes made to the admin dashboard

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = 'Username'
+
+    list_display = ('get_username', 'title', 'description', 'status', 'deadline')
+    
+admin.site.register(Task, TaskAdmin)
